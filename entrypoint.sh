298cfa47ef4d36d5931d54fd590997e886e44938
Guillermo Caracuel <guillermo.caracuel@technest.es>
2019-09-25 16:05:46 +0200

    Fixes entrypoint override to MONGO_PASSWORD

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 if [[ -v PASSWORD_LOCATION ]]; then
-  export REMCO_PASSWORD=$(cat $PASSWORD_LOCATION)
+  export MONGO_PASSWORD=$(cat $PASSWORD_LOCATION)
   echo "Using \$PASSWORD_LOCATION value"
-elif [[ -v REMCO_PASSWORD ]]; then
-  echo "Using \$REMCO_PASSWORD value"
+elif [[ -v MONGO_PASSWORD ]]; then
+  echo "Using \$MONGO_PASSWORD value"
 else
-  echo "ERROR! Neither PASSWORD nor PASSWORD_LOCATION has been provided"
+  echo "ERROR! Neither MONGO_PASSWORD nor PASSWORD_LOCATION has been provided"
   exit 1
 fi
 
